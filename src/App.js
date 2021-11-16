cf15d08dd65c8c321b82061ecb2b11591ed275ea
Mathias Gonzalez <lpthemaster@hotmail.com>
2021-11-16 14:37:18 -0300

    probando

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    fetch('http://localhost:54321/infoDevice')
+          .then(responde => responde.json())
+          .then(data => console.log(`Prueba ${JSON.stringify(data)}`))
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:12345/?printer=2;;N;;Documenta')
+        .then(response => response.json())
+        .then(data => console.log(`Ticket ${JSON.stringify(data)}`))
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
